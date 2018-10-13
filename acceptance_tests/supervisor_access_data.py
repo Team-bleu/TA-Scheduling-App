03d7fe5b30483e576a6f63e67cd941dd064a5956
Yangbee Thao <yangbeethao@gmail.com>
2018-10-12 19:48:00 -0500

    added supervisor checking data and information in the system

---
+++ acceptance_tests/supervisor_access_data.py
@@ -0,0 +1,10 @@
+# As a Supervisor, I want to access all
+# of the data in the system
+
+import unittest
+from app import App
+
+
+class SupervisorCheckInfo(unittest.TestCase):
+    def test_supervisor_check_info(self):
+        print("\nAs a Supervisor, I want to access\nall of the data in the system")
