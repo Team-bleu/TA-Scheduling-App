5e35236f0379903736b38290922497ff56b6e753
Yangbee Thao <yangbeethao@gmail.com>
2018-10-21 11:30:37 -0500

    added admin sending notifications

---
+++ acceptance_tests/admin_send_notifications.py
@@ -0,0 +1,26 @@
+import unittest
+from app import App
+
+
+# 13.
+# As an Administrator, I want to send out notifications
+# via email, so that I can update people with new information
+class SupAssignTALab(unittest.TestCase):
+    app = App()
+
+    # First we have the Supervisor, Luke, login
+    # with <username>: Luke and <password>: password
+    def test_login(self):
+        self.assertEqual(self.app.command("login Luke password"), "Luke logged in")
+
+    # Then we have Luke assign the TA, John, the course, CS250
+    def test_assign_course(self):
+        self.assertEqual(self.app.command("assigncourse John CS250"), "John has been added to the course")
+
+    # Then we assign the TA, John, a lab section from that course, Sec801
+    def test_assign_course(self):
+        self.assertEqual(self.app.command("assignlab  John Sec801"), "John has been added to Sec801")
+
+    # Then we have the Supervisor logout
+    def test_logout(self):
+        self.assertEqual(self.app.command("logout"), "Luke logged out")
