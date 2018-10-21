8f3ac49ab992e4baec38d6eb12f4f0b33006d644
Yangbee Thao <yangbeethao@gmail.com>
2018-10-21 11:52:11 -0500

    added tests

--- acceptance_tests/main_tests.py
+++ acceptance_tests/main_tests.py
@@ -1,13 +1,23 @@
 import unittest
-from supervisor_assign_TA_lab import SupAssignTALab
-
+import supervisor_assign_TA_lab
+import supervisor_show_TA
+import admin_send_notifications
+import instructor_edit_info
+import instructor_assign_TA_lab
 
 
 def main_tests():
     suite = unittest.TestSuite()
 
-    suite.addTest(unittest.makeSuite())
+    suite.addTest(unittest.makeSuite(supervisor_assign_TA_lab.SupAssignTALab))
+    suite.addTest(unittest.makeSuite(supervisor_show_TA.SupShow))
+    suite.addTest(unittest.makeSuite(admin_send_notifications.AdminNotify))
+    suite.addTest(unittest.makeSuite(instructor_edit_info.InstructEdit))
+    suite.addTest(unittest.makeSuite(instructor_assign_TA_lab.InstructAssignTALab))
 
     runner = unittest.TextTestRunner()
     res = runner.run(suite)
     print(res)
+
+if __name__ == "__main__":
+    main_tests()
