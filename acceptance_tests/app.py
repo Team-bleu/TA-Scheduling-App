66b2e009b1e26621f0cc3505228c6fd0590dc0a8
Yangbee Thao <38472231+SirVanish@users.noreply.github.com>
2018-10-22 17:26:08 -0500

    Update app.py

--- acceptance_tests/app.py
+++ acceptance_tests/app.py
@@ -6,10 +6,7 @@ class App:
     labs = []
 
     def command(self, userstring):    # userString is what the user typed on the command line
-        return ""               # the return value will be the applications response
-    def login(self, userName, password):
-        return ""
-    def logout(self):
-        return
+        return ""                     # the return value will be the applications response
+
     def done(self):
         return True;  # will let user know that they're done
