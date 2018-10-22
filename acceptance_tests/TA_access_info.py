16a568a47c4647e620a5b18f2fa3ab13e4ebfb88
xmr1996 <xiangm@uwm.edu>
2018-10-22 18:02:52 -0500

    testing

---
+++ acceptance_tests/TA_access_info.py
@@ -0,0 +1,26 @@
+
+
+import unittest
+from app import App
+
+
+# 23 As an TA, I want to read public contact information of the users
+#All public contact information from the TA's and other instructors should be print on the screen
+class TestSupCreateAccount(unittest.TestCase):
+    app = App()
+    # First we have the instructor login
+    def test_login(self):
+        self.assertEqual(self.app.command("login Luke password"), "Luke logged in")
+    # nest we have the Instructor call view on John
+    # Then we delete an account
+    infor = {"username": "John", "role": "TA", "phone": "999-930-9999", "email":"john@uwm.edu", "course" : "cs250",
+             "lab": "section802"}
+
+    def test_view(self):
+        self.assertEqual(self.app.command("view John"), self.infor)
+
+
+    # Then we have Instructor logout
+    def test_logout(self):
+        self.assertEqual(self.app.command("logout"), "Luke logged out")
+
---
+++ acceptance_tests/supervisor_login_logout.py
@@ -0,0 +1,16 @@
+import unittest
+from app import App
+
+
+#7 a Supervisor, I want to have a user interface so that I can login and logout my account
+class TestSupCreateAccount(unittest.TestCase):
+    app = App()
+
+    # First we have the Supervisor login
+    def test_login(self):
+        self.assertEqual(self.app.command("login Luke password"), "Luke logged in")
+
+    # Then we have them logout
+    def test_logout(self):
+        self.assertEqual(self.app.command("logout"), "Luke logged out")
+
