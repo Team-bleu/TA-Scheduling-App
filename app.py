eb26b2c542977a27721950bb7f3f34567117aef3
Yangbee Thao <yangbeethao@gmail.com>
2018-10-23 16:42:22 -0500

    updated app

--- app.py
+++ app.py
@@ -11,7 +11,12 @@ class App:
     # user_input is what the user typed on the command line
     # the return value will be the application's response
     def command(self, user_input):
-        print(self.commands.check_command(user_input, self.users, self.courses, self.labs))
+        response = self.commands.check_command(user_input, self.users, self.courses, self.labs)
+
+        # This checks if response is a string, if not then, print nothing
+        # (otherwise, it'll print None every time an incorrect command is used
+        if response:
+            print(response)
 
     def done(self):
         return True  # will let user know that they're done
