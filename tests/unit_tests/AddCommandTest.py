4c86184d1bb18699bde5853875eaae8dd7263786
xmr1996 <xiangm@uwm.edu>
2018-11-01 07:14:43 -0500

    unit test for two command

--- tests/unit_tests/AddCommandTest.py
+++ tests/unit_tests/AddCommandTest.py
@@ -1,5 +1,32 @@
 import unittest
+from AddCommand import AddCommand
 
 
 class AddCommandTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.cmd = AddCommand()
+        self.user_input_list1 = ["add", "user1", "admin123"]
+        self.user_input_list2 = ["add", "user2", "admin123"]
+        self.user_input_list3 = ["add", "user3", "admin123"]
+        self.invalid_input_list = ["invalidCommand", "user"]
+
+    def test_invalid_input_list(self):
+        self.assertTrue(self.cmd.countArgs(self.invalid_input_list))
+        self.assertFalse(self.cmd.isCommand(self.invalid_input_list[0]))
+
+    def test_valid_input_list(self):
+        self.assertFalse(self.cmd.countArgs(self.user_input_list1))
+        self.assertFalse(self.cmd.countArgs(self.user_input_list2))
+        self.assertTrue(self.cmd.isCommand(self.user_input_list1[0]))
+        self.assertTrue(self.cmd.isCommand(self.user_input_list2[0]))
+
+    def test_add_user(self):
+        self.assertEqual(self.cmd.action(self.user_input_list1, "", "", ""), "user1 has been added")
+        self.assertEqual(self.cmd.action(self.user_input_list1, "", "", ""), "User already exists.")
+        self.assertEqual(self.cmd.action(self.user_input_list2, "", "", ""), "user1 has been added")
+        self.assertEqual(self.cmd.action(self.user_input_list2, "", "", ""), "User already exists.")
+
+
+if __name__ == "__main__":
+    unittest.main()
