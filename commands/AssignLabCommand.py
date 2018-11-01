2f49c96093492ead2e828b9176f2c5a30db96b4c
markcortez <mlcortez@uwm.edu>
2018-11-01 12:44:40 -0500

    added CourseUtility and some other files, NOT DONE YET

---
+++ commands/AssignLabCommand.py
@@ -0,0 +1,13 @@
+from Command import Command
+
+
+class AssignLabCommand(Command):
+
+    def action(self, user_input_list, user, courses, labs):
+        pass
+
+    def isCommand(self, command):
+        return command == "assignlab"
+
+    def countArgs(self, user_input_list):
+        return len(user_input_list) < 3
--- commands/CreateCourseCommand.py
+++ commands/CreateCourseCommand.py
@@ -1,10 +1,17 @@
 from Command import Command
+from CourseUtility import CourseUtility
 
 
 class CreateCourseCommand(Command):
 
     def action(self, user_input_list, user, courses, labs):
-        pass
+
+        courseUtil = CourseUtility()
+        courseName = user_input_list[1]
+
+
+        courseUtil.createCourse(courseName)
+
 
     def isCommand(self, command):
         return command == "createcourse"
