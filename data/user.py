ec72a07a8be2afd37ce7db369b0d643053da5077
Yangbee Thao <yangbeethao@gmail.com>
2018-11-04 16:44:40 -0600

    updated unittests and implementation of BSTUtil to return None during searches for none-existing users

--- data/user.py
+++ data/user.py
@@ -1,14 +1,14 @@
 
 
 class User:
-    def __init__(self, firstname="None", lastname="None", username="None", password="None",
-                role="None", information="None", course="None", lab="None",
-                assignment="None", parent="None", leftChild="None", rightChild="None"):
+    def __init__(self, firstname=None, lastname=None, username=None, password=None,
+                role=None, phone=None, email=None, address=None, course=None, lab=None,
+                assignment=None, parent=None, leftChild=None, rightChild=None):
         self.setName(firstname, lastname)
         self.setAccount(username, password)
         self.setRole(role)
         self.setClass(course, lab)
-        self.setInfo(information)
+        self.setInfo(phone, email, address)
         self.setAssignment(assignment)
         self.setParent(parent)
         self.setLeftChild(leftChild)
@@ -19,9 +19,13 @@ class User:
         self.lastname = lastname
 
     def getFirstName(self):
+        if self.firstname == "None":
+            return None
         return self.firstname
 
     def getLastName(self):
+        if self.lastname == "None":
+            return None
         return self.lastname
 
     def setAccount(self, username, password):
@@ -29,15 +33,21 @@ class User:
         self.password = password
 
     def getUsername(self):
+        if self.username == "None":
+            return None
         return self.username
 
     def getPassword(self):
+        if self.password == "None":
+            return None
         return self.password
 
     def setRole(self, role):
         self.role = role
 
     def getRole(self):
+        if self.role == "None":
+            return None
         return self.role
 
     def setClass(self, course, lab):
@@ -45,42 +55,86 @@ class User:
         self.lab = lab
 
     def getCourse(self):
+        if self.course == "None":
+            return None
         return self.course
 
     def getLab(self):
+        if self.lab == "None":
+            return None
         return self.lab
 
-    def setInfo(self, information):
-        self.information = information
+    def setInfo(self, phone, email, address):
+        self.setPhone(phone)
+        self.setEmail(email)
+        self.setAddress(address)
 
     def getInfo(self):
-        return self.information
+        return [self.getPhone(), self.getEmail(), self.getAddress()]
+
+    def setPhone(self, phone):
+        self.phone = phone
+
+    def setEmail(self, email):
+        self.email = email
+
+    def setAddress(self, address):
+        self.address = address
+
+    def getPhone(self):
+        if self.phone == "None":
+            return None
+        return self.phone
+
+    def getEmail(self):
+        if self.email == "None":
+            return None
+        return self.email
+
+    def getAddress(self):
+        if self.address == "None":
+            return None
+        return self.address
 
     def setAssignment(self, assignment):
         self.assignment = assignment
 
     def getAssignment(self):
+        if self.assignment == "None":
+            return None
         return self.assignment
 
     def setParent(self, parent):
         self.parent = parent
 
     def getParent(self):
+        if self.parent == "None":
+            return None
+        if isinstance(self.parent, User):
+            self.parent = self.parent.getUsername() + ".txt"
         return self.parent
 
     def setLeftChild(self, leftChild):
         self.leftChild = leftChild
 
     def getLeftChild(self):
+        if self.leftChild == "None":
+            return None
+        if isinstance(self.leftChild, User):
+            self.leftChild = self.leftChild.getUsername() + ".txt"
         return self.leftChild
 
     def setRightChild(self, rightChild):
         self.rightChild = rightChild
 
     def getRightChild(self):
+        if self.rightChild == "None":
+            return None
+        if isinstance(self.rightChild, User):
+            self.rightChild = self.rightChild.getUsername() + ".txt"
         return self.rightChild
 
     def getContents(self):
         return [self.getFirstName(), self.getLastName(), self.getUsername(), self.getPassword(), self.getRole(),
-                self.getInfo(), self.getCourse(), self.getLab(), self.getAssignment(),
+                self.getPhone(), self.getEmail(), self.getAddress(), self.getCourse(), self.getLab(), self.getAssignment(),
                 self.getParent(), self.getLeftChild(), self.getRightChild()]
--- tests/unit_tests/BSTUtility_test.py
+++ tests/unit_tests/BSTUtility_test.py
@@ -9,9 +9,12 @@ class BSTUtilityTest(unittest.TestCase):
     def setUp(self):
         self.util = BSTUtility()
         self.user1 = User("first", "last", "user1", "admin123", "role",
-                          ["phone", "email", "address"], "course", "lab", "assignment", "None", "None", "None")
+                          "phone", "email", "address", "course", "lab", "assignment", "None", "None", "None")
+        self.searchuser = User("first", "last", "searchuser", "password", "role",
+                          "phone", "email", "address", "course", "lab", "assignment", "None", "None", "None")
         self.emptyUser = User()
 
+        self.util.updateUser(self.searchuser)
 
 
     def test_append(self):
@@ -34,47 +37,53 @@ class BSTUtilityTest(unittest.TestCase):
         self.assertIsInstance(self.util.createUser(test1), User)
         self.assertEqual(self.util.createUser(test1).getContents(), self.user1.getContents())
 
-
-    # I am not sure about this one, if left child is none,
-    # shouldn't the method return none instead of an empty user?
-    # Also, could you help me add the unit test for testing left child if the current does have left child
-    # I am not sure how to set the left child properly
     def test_get_left_child(self):
         self.assertIsInstance(self.util.getLeftChild(), User)
         self.assertEqual(self.util.getLeftChild().getContents(), self.emptyUser.getContents())
 
-    # same issue as the test left child for this one
     def test_get_right_child(self):
         self.assertIsInstance(self.util.getRightChild(), User)
         self.assertEqual(self.util.getRightChild().getContents(), self.emptyUser.getContents())
 
     #If user does't exist. would it be better to return None than an empty user?
     def test_search_user(self):
-        self.assertIsInstance(self.util.searchUser("NotExist"), User)
-        self.assertEqual(self.util.searchUser("NotExist").getContents(), self.emptyUser.getContents())
-        self.util.addUser(self.user1)
-        self.assertIsInstance(self.util.searchUser("user1"), User)
-        #this one should not fail, I add user1 and search for user1 but it still failed, not sure why
-        self.assertEqual(self.util.searchUser("user1").getContents(), self.user1.getContents())
+        self.assertEqual(self.util.searchUser("NotExist"), None)
+
+        # We must reset the utility before looking for the new user
+        self.util.setUp()
+        self.assertIsInstance(self.util.searchUser("searchuser"), User)
+        self.assertEqual(self.util.searchUser("searchuser").getContents(), self.searchuser.getContents())
+        # Now we must reset the utility and remove the dummy user
+        # But the line below doesn't work. Manually delete searchuser.txt
+        # and update the neighbors.
+        self.assertIsNone(self.util.removeUser("searchuser"))
 
     # this one is not finsihed
     def test_remove_user(self):
         self.assertIsNone(self.util.removeUser("NotExist"))
 
-    # here is the method I left out. Sorry about that. I understand how the code works
-    # but Can't think about a way to properly test them since they don't return anything
-    # It would be great if you can help me implement them
+    # I'm not sure if we can test an initialization of the utility object
+    # without adding getters for the dataset
     def test_init(self):
         pass
-    def test_add_user(self):
-        pass
+
     def test_update_user(self):
-        pass
+        self.util.updateUser(self.searchuser)
+        # If the user "searchuser" has been added correctly, we can verify its contents
+        self.assertEqual(self.util.searchUser("searchuser").getContents(), self.searchuser.getContents())
+
+    def test_add_user(self):
+        self.util.addUser(self.searchuser)
+        # If the user "searchuser" has been added correctly, we can verify its contents
+        # same reason as update user test
+        self.assertEqual(self.util.searchUser("searchuser").getContents(), self.searchuser.getContents())
+
     def test_setup_root(self):
-        pass;
+        self.assertIsInstance(self.util.setUpRoot(), User)
+        # Here, our root will be super.txt
+        self.assertEqual(self.util.setUpRoot().getUsername() + ".txt", "super.txt")
 
     def tearDown(self):
-
         self.util = None
         self.user1 = self.emptyUser = None
 
