221002f121332a478df316b9489169a6e896ebc1
xmr1996 <xiangm@uwm.edu>
2018-11-04 01:49:03 -0600

    Need help with BSTutility test

--- tests/unit_tests/LoginCommandTest.py
+++ tests/unit_tests/LoginCommandTest.py
@@ -1,5 +1,21 @@
 import unittest
-
+from LoginCommand import LoginCommand
 
 class LoginCommandTest(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.cmd = LoginCommand()
+        self.user_input_list1 = ["login", "user1", "admin123"]
+        self.user_input_list2 = ["login", "user2", "admin123"]
+        self.user_input_list3 = ["login", "user3", "admin123"]
+        self.invalid_input_list = ["invalidCommand", "user"]
+
+    def test_invalid_input_list(self):
+        self.assertTrue(self.cmd.countArgs(self.invalid_input_list))
+        self.assertFalse(self.cmd.isCommand(self.invalid_input_list[0]))
+
+    def test_valid_input_list(self):
+        self.assertFalse(self.cmd.countArgs(self.user_input_list1))
+        self.assertFalse(self.cmd.countArgs(self.user_input_list2))
+        self.assertTrue(self.cmd.isCommand(self.user_input_list1[0]))
+        self.assertTrue(self.cmd.isCommand(self.user_input_list2[0]))
