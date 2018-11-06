4233c2532dbf94950439eaa5df9fc6a7d2c3e514
Yangbee Thao <yangbeethao@gmail.com>
2018-11-05 19:31:40 -0600

    fix the unit test

--- tests/unit_tests/LoginCommandTest.py
+++ tests/unit_tests/LoginCommandTest.py
@@ -7,14 +7,6 @@ from user import User
 class LoginCommandTest(unittest.TestCase):
 
     def setUp(self):
-        # We must have the utility so we can add a
-        # dummy user in order to login.
-        self.util = BSTUtility()
-        # Now we must create the dummy user
-        self.user = User()
-        self.user.setAccount("user1", "admin123")
-        # And we add the dummy user
-        self.util.addUser(self.user)
 
         self.cmd = LoginCommand()
         self.user_input_list1 = ["login", "user1", "admin123"]
@@ -33,6 +25,15 @@ class LoginCommandTest(unittest.TestCase):
         self.assertTrue(self.cmd.isCommand(self.user_input_list2[0]))
 
     def test_login_command(self):
+        # We must have the utility so we can add a
+        # dummy user in order to login.
+        self.util = BSTUtility()
+        # Now we must create the dummy user
+        self.user = User()
+        self.user.setAccount("user1", "admin123")
+        # And we add the dummy user
+        self.util.updateUser(self.user)
+
         self.assertEqual(self.cmd.action(self.user_input_list1), "user1 logged in.")
         self.assertEqual(self.cmd.action(self.user_input_list2), "Wrong password.")
         self.assertEqual(self.cmd.action(self.user_input_list3), "No such user exists.")
