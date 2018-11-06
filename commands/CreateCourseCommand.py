c35ad933d86466f5124fd5e86bbd89e5bd331e0b
Yangbee Thao <38472231+SirVanish@users.noreply.github.com>
2018-11-05 18:24:40 -0600

    Update CreateCourseCommand.py
    revert

--- commands/CreateCourseCommand.py
+++ commands/CreateCourseCommand.py
@@ -7,6 +7,13 @@ class CreateCourseCommand(Command):
     def action(self, user_input_list, user, courses, labs):
         if not CommandsList.getCredentialss() >= 3:
             return "Error. Current User does not have permission to create courses."
+        
+        courseUtil = CourseUtility()
+        courseName = user_input_list[1]
+
+
+        courseUtil.createCourse(courseName)
+
 
     def isCommand(self, command):
         return command == "createcourse"
