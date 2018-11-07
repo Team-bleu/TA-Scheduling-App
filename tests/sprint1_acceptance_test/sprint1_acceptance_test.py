c7b854d8952f93f187af377ebe609cd0d89c460e
xmr1996 <xiangm@uwm.edu>
2018-11-06 19:33:14 -0600

    Done!!!!!!!!

--- tests/sprint1_acceptance_test/sprint1_acceptance_test.py
+++ tests/sprint1_acceptance_test/sprint1_acceptance_test.py
@@ -12,7 +12,7 @@ class acceptanceTest(unittest.TestCase):
         self.assertEqual(self.app.command("login super pass"), "super logged in.")
         self.assertEqual(self.app.command("login super 123"), "Wrong password.")
         self.assertEqual(self.app.command("login user pass"), "No such user exists.")
-
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     # 7(Sprint 1) a Supervisor, I want to have a user interface so that I can login and logout my account
     def test_logout(self):
@@ -25,12 +25,14 @@ class acceptanceTest(unittest.TestCase):
         self.assertEqual(self.app.command("add user1 admin123"), "user1 has been added")
         self.assertEqual(self.app.command("add user1 admin123"), "User already exists.")
         self.assertEqual(self.app.command("add user1"), "Not enough arguments.")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     # 2(Sprint 1) As a supervisor I want to create a new course so that I can assign TA's and instructors.
     def test_supervisor_create_course(self):
         self.assertEqual(self.app.command("login super pass"), "super logged in.")
         self.assertEqual(self.app.command("createcourse CS361"), "CS361 has been created")
         self.assertEqual(self.app.command("createcourse"), "Not enough arguments.")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     # 2(Sprint 1) As a supervisor I want to create a new course so that I can assign TA's and instructors.
     def test_non_supervisor_create_course(self):
@@ -45,12 +47,14 @@ class acceptanceTest(unittest.TestCase):
         self.assertEqual(self.app.command("createcourse CS351"), "CS351 has been created")
         self.assertEqual(self.app.command("assigncourse TA1 CS351"), "TA1 has been assigned to CS351")
         self.assertEqual(self.app.command("assigncourse TA1"), "Not enough arguments.")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     # 8(Sprint 1) As a supervisor I want to assign TAs to courses so that later, the TAs can be assigned to a lab section
     def test_non_supervisor_assign_course(self):
         self.assertEqual(self.app.command("login user1 admin123"), "user1 logged in.")
         self.assertEqual(self.app.command("createcourse CS351"), "Do not have permission")
         self.assertEqual(self.app.command("assigncourse TA1 CS351"), "Do not have permission")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     # 5(Sprint 1) As a supervisor I want to assign TAs to particular lab sections so that they know what lab to teach.
     def test_supervisor_create_lab(self):
@@ -58,12 +62,14 @@ class acceptanceTest(unittest.TestCase):
         self.assertEqual(self.app.command("createlab CS361 lab801"), "lab801 has been created")
         self.assertEqual(self.app.command("createlab CS361 lab802"), "lab802 has been created")
         self.assertEqual(self.app.command("createlab CS361"), "Not enough arguments.")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     # 5(Sprint 1) As a supervisor I want to assign TAs to particular lab sections so that they know what lab to teach.
     def test_non_supervisor_create_lab(self):
         self.assertEqual(self.app.command("login user1 admin123"), "user1 logged in.")
         self.assertEqual(self.app.command("createlab CS361 lab801"), "Do not have permission")
         self.assertEqual(self.app.command("createlab CS251 lab801"), "Do not have permission")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     # 5(Sprint 1) As a supervisor I want to assign TAs to particular lab sections so that they know what lab to teach.
     def test_supervisor_assign_lab(self):
@@ -72,10 +78,12 @@ class acceptanceTest(unittest.TestCase):
         self.assertEqual(self.app.command("createlab CS351 lab802"), "lab802 has been created")
         self.assertEqual(self.app.command("assignlab TA1 CS351 LAB801"), "TA1 has been assigned to LAB801")
         self.assertEqual(self.app.command("assignlab CS361"), "Not enough arguments.")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
     def test_non_supervisor_assign_lab(self):
         self.assertEqual(self.app.command("login user1 admin123"), "user1 logged in.")
         self.assertEqual(self.app.command("assignlab TA1 CS351 LAB801"), "Do not have permission")
+        self.assertEqual(self.app.command("logout"), "logged out.")
 
 
 
