3351c62d9875ec1af37c540fa049f4bf70f6af39
Yangbee Thao <yangbeethao@gmail.com>
2018-11-15 15:04:28 -0600

    simplified views code

--- django/main/views.py
+++ django/main/views.py
@@ -2,53 +2,13 @@ from django.shortcuts import render
 from django.views import View
 from app import App
 
-def addUser(args):
-  #if args[0] == addUser, adds user named args[1]
-  #returns "User <args[1]> added" or "User <args[1]> exists"
-  #else returns ""
-  if args[0] == "addUser":
-    return "I am addUser"
-  else:
-     return ""
-
-def addItem(args):
-  #if args[0] == "addItem", adds item args[1] to user args[2]
-  #returns "Item <args[1]> added to user <args[2]>" 
-  #or "User <args[2]> already has item <args[1]>"
-  #or "User <args[2]> does not exist"
-  #else returns ""
-  if args[0] == "addItem":
-    return "I am addItem"
-  else:
-     return ""
-
-def display(args):
-  #if args[0] == display
-  #return a string with each user followed by their items
-  #items are indented
-  if args[0] == "display":
-    return "I am display"
-  else:
-     return ""
-
-commandList = [addUser, addItem, display]
-def doStuff(s, commandList):
-  args = s.split(" ")
-  for i in commandList:
-    out = i(args)
-    if out != "": #if i matches arg[0], stop looping
-      break
-  if out == "":
-    out = "command not found"
-  return out
 
 # Create your views here.
-
 class Home(View):
-  def get(self,request):
+  def get(self, request):
     return render(request, "main_temp/index.html")
-  def post(self,request):
-#    out = doStuff(request.POST["command"],commandList)
+
+  def post(self, request):
     app = App()
     out = app.command(request.POST["command"])
-    return render(request, "main_temp/index.html", {"out":out})
+    return render(request, "main_temp/index.html", {"out": out})
