4fb943901612b8c0a4e1e35f67e4abc9fe9abe93
Yangbee Thao <yangbeethao@gmail.com>
2018-12-09 18:51:12 -0600

    added unimplemented sprint 3 acceptance tests

---
+++ TA_App/tests/AcceptanceTest/test_sprint3_acceptance_test.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from application.app import App
+
+
+# test for sprint 3
+# https://tree.taiga.io/project/ogarcia27-ta-scheduling-app/taskboard/sprint-2-1328
+class acceptanceTest3(TestCase):
+    app = App()
+
+    def test_userstory_18(self):
+        pass
+
+    def test_userstory_13(self):
+        pass
+
+    def test_userstory_10(self):
+        pass
+
+    def test_userstory_22(self):
+        pass
+
+    def test_userstory_20(self):
+        pass
