386f794b3a7adad39c7304c38822bf8f1daf7448
markcortez <markcortez94@gmail.com>
2018-12-12 12:02:47 -0600

    fixed some bugs with changing Instructor role

--- application/commands/RoleCommand.py
+++ application/commands/RoleCommand.py
@@ -1,4 +1,5 @@
 from UserUtility import UserUtility
+from CourseUtility import CourseUtility
 from Command import Command
 
 
@@ -15,6 +16,7 @@ class RoleCommand(Command):
         username = user_input_list[1]
         role = user_input_list[2]
         util = UserUtility()
+        courseUtil = CourseUtility()
 
         if not RoleCommand.checkRole(self, role):
             return role + " doesn't exist!"
@@ -23,6 +25,20 @@ class RoleCommand(Command):
 
         if user is None:
             return username + " doesn't exist!"
+
+        # For removing Instructor Database file and unassigning this Instructor from any classes they had
+        if (user.getRole() == "Instructor"):
+            if (role != "Instructor"):  # if role is getting changed from Instructor to something else
+                courses = user.getCourses()
+                if courses[0] != "None":
+                    for i in range(0, courses.__len__()): #unassign this Instructor from all courses
+                        courseUtil.getContents(courses[i])
+                        user.unAssignCourse(courses[i])     #unassign courses in user file
+                        courseUtil.unAssignCourse(username) #Unassign instructor from course file
+                        courseUtil.writeContents()
+
+                courseUtil.removeDBInstructor(user.getUsername()) #delete database file
+
 
         user.setRole(role)
         util.updateUser(user)
